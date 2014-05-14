11dacf8f6a65e2b8fcc896768fcc503eb0aa6e56
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-05-14 11:23:39 +0800

    add yc_geo_get_city_info test

--- 1.12/Zend_Db/test.php
+++ 1.12/Zend_Db/test.php
@@ -1,4 +1,8 @@
 <?php
+error_reporting(E_ALL);
+
+println(yc_geo_get_city_info("bj"));
+
 require_once 'Zend/Db.php';
 
 $params = array(
@@ -9,6 +13,10 @@ $params = array(
 );
 
 $db = Zend_Db::factory('PDO_MYSQL', $params);
+
+// 为所有的Zend_Db_Table对象设定默认的adapter
+require_once 'Zend/Db/Table.php';
+Zend_Db_Table::setDefaultAdapter($db);
 
 $select = $db -> select();
 // $select现在是一个Zend_Db_Select_PdoMysql对象
@@ -33,10 +41,15 @@ $select -> limit(1, 0);
 // 读取数据的方法相同
 $sql = $select -> __toString();
 $result = $db -> fetchAll($sql);
-print_r($result);
-var_dump($result);//显示数据结构
+println($result);
+println($result);//显示数据结构
 //error_log($result);
 //var_export();
 
 // 对于以上任一种方式，都可以传递$select对象本身
 // 使用Zend_Db_Select对象的__toString()可以得到查询语句
+
+function println($data) {
+    print_r($data);
+    echo "<br />";
+}
